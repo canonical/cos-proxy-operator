590459c981026ba9b740435e54e56e1df0eb42f8
Simon Aronsson <simme@arcticbit.se>
2023-02-24 08:44:54 +0100

    Update nrpe_exporter.py

--- lib/charms/nrpe_exporter/v0/nrpe_exporter.py
+++ lib/charms/nrpe_exporter/v0/nrpe_exporter.py
@@ -433,7 +433,8 @@ class NrpeExporterProvider(Object):
             "alert": "{}NrpeAlert".format("".join([x.title() for x in cmd.split("_")])),
             # Average over 5 minutes considering a 60 second scrape interval
             "expr": "avg_over_time(command_status{{juju_unit='{}',command='{}'}}[5m]) > 1".format(
-                re.sub(r"^(.*?)[-_](\d+)$", r"\1/\2", id.replace("_", "-"), cmd)
+                re.sub(r"^(.*?)[-_](\d+)$", r"\1/\2", id.replace("_", "-")),
+                cmd
             ),
             "for": "0m",
             "labels": {
